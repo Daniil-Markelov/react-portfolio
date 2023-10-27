680d7917487c86cad1b4637545ff7753e10bfcf8
daniil markelov <N00202705@student.iadt.ie>
2023-10-27 16:48:25 +0100

    cant seem to make the cards wrap in a grid!

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,26 +1,16 @@
-import {Card} from 'react-bootstrap';
-import {Row, Col} from'react-bootstrap';
-import {Link} from'react-router-dom';
+import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
 const CountryCard = (props) => {
-
     return (
-        <Row>
-            <Col>
-                    <Card className="country-card">
-            <Card.Body>
-            <Card.Title>
-  <Link to={`/country/${props.name}`}>{props.name}</Link>
-</Card.Title>
-
-                <Card.Img variant="top" src={props.flag} alt={props.name}  style={{maxWidth: '200px'}}/>
-            </Card.Body>
-        </Card>
-            
-            </Col>
-        </Row>
-
+      <Card className="country-card">
+        <Card.Img variant="top" src={props.flag} />
+        <Card.Body>
+          <Card.Title>
+            <Link to={`/country/${props.name}`} className="country-link">{props.name}</Link>
+          </Card.Title>
+          <Card.Text>{props.region}</Card.Text>
+        </Card.Body>
+      </Card>
     );
-
-};
-
-export default CountryCard;
+  };
+export default CountryCard
